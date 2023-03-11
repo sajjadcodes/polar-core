033dd06a65ace515851f3fee636a1c777cad2e94
sajjadcodes <sajjadcodes@gmail.com>
2023-03-11 06:28:28 +0500

    plugin header added

---
+++ polar-core.php
@@ -0,0 +1,13 @@
+<?php 
+/*
+ * Plugin Name:       Polar Core
+ * Plugin URI:        https://Polar.com/
+ * Description:       Polar blog site core functionalities
+ * Version:           1.10.3
+ * Requires at least: 5.2
+ * Requires PHP:      7.2
+ * Author:            Sajad Hussain
+ * Author URI:        https://sajjadcodes.com/
+ * Text Domain:       polar-core
+ * Domain Path:       /languages
+ */
